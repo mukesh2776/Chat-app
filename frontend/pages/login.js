25e9cebd7dbd3eda39d72b1b16d5a713a10ba8ef
mukesh2776 <mukeshsr943@gmail.com>
2025-07-04 18:07:18 +0530

    makes changes in res

--- frontend/pages/login.js
+++ frontend/pages/login.js
@@ -10,7 +10,7 @@ export default function Login() {
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/login`, { email, password });
+      const res =await axios.post(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/login`, { email, password });
       localStorage.setItem('token', res.data.token);
       localStorage.setItem('username', res.data.user.username);
       router.push('/chat');
